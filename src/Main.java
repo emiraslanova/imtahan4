55b3f22986bb2fdba6cf992a2e88855baaf5cb41
Mina <amiraslanovamina@gmail.com>
2023-05-03 22:00:50 +0400

    map

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,19 @@ public class Main {
     private static Map<Integer, String> map = new HashMap<>();
 
     public static void main(String[] args) {
-
-
-    }
-
-    public static void map(String str) {
-        String[] s = str.split("");
-        for (int i = 0; i < s.length; i++) {
-            map.put(i,s[i]);
+        String s = "aaa aa bbb ccc aa aaaa bbb";
+        String[] sa = s.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String s1: sa) {
+            map.put(s1, map.getOrDefault(s1, 0) + 1);
         }
+        for (String word:
+             map.keySet()) {
+            System.out.println(word+ " " +map.get(word));
+        }
+    }
+
 
 
     }
-}
+
